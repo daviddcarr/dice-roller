02717eb022b67024d1e0f4b0a9a3155dfc798b3b
daviddcarr <daviddcarr13@gmail.com>
2023-04-27 13:00:35 -0500

    Update UI and add history modal.

---
+++ src/components/Interface.jsx
@@ -0,0 +1,123 @@
+import { useState, useMemo } from 'react'
+
+export default function Interface({ throwingDice, throwDice, clearDice, total, totals, dice }) {
+
+    const [ showTotals, setShowTotals ] = useState(false)
+
+    const calculateDiceRolls = (dice) => {
+        let diceCounts = {
+            4: 0,
+            6: 0,
+            8: 0,
+            10: 0,
+            12: 0,
+            20: 0,
+        }
+
+        dice.forEach((die) => {
+            diceCounts[die.sides]++
+        })
+
+        let diceString = ''
+
+        for (const [key, value] of Object.entries(diceCounts)) {
+            if (value > 0) {
+                diceString += `${value}D${key} `
+            }
+        }
+
+        return diceString
+    }
+
+    return (
+        <div className="absolute inset-0  pointer-events-none">
+            <div className="w-full p-4 flex items-center justify-center">
+                <div className='flex space-x-4'>
+                    <button 
+                        className={`pointer-events-auto h-12 w-12 ${ throwingDice ? 'bg-gray-400 text-gray-500' : 'bg-gray-800 hover:bg-purple-500 text-white' } rounded-full`}
+                        disabled={throwingDice}
+                        onClick={() => throwDice('4')}
+                        >
+                        D4
+                    </button>
+                    <button
+                        className={`pointer-events-auto h-12 w-12 ${ throwingDice ? 'bg-gray-400 text-gray-500' : 'bg-gray-800 hover:bg-purple-500 text-white' } rounded-full`}
+                        disabled={throwingDice}
+                        onClick={() => throwDice('6')}
+                        >
+                        D6
+                    </button>
+                    <button
+                        className={`pointer-events-auto h-12 w-12 ${ throwingDice ? 'bg-gray-400 text-gray-500' : 'bg-gray-800 hover:bg-purple-500 text-white' } rounded-full`}
+                        disabled={throwingDice}
+                        onClick={() => throwDice('8')}
+                        >
+                        D8
+                    </button>
+                    <button
+                        className={`pointer-events-auto h-12 w-12 ${ throwingDice ? 'bg-gray-400 text-gray-500' : 'bg-gray-800 hover:bg-purple-500 text-white' } rounded-full`}
+                        disabled={throwingDice}
+                        onClick={() => throwDice('10')}
+                        >
+                        D10
+                    </button>
+                    <button
+                        className={`pointer-events-auto h-12 w-12 ${ throwingDice ? 'bg-gray-400 text-gray-500' : 'bg-gray-800 hover:bg-purple-500 text-white' } rounded-full`}
+                        disabled={throwingDice}
+                        onClick={() => throwDice('12')}
+                        >
+                        D12
+                    </button>
+                    <button
+                        className={`pointer-events-auto h-12 w-12 ${ throwingDice ? 'bg-gray-400 text-gray-500' : 'bg-gray-800 hover:bg-purple-500 text-white' } rounded-full`}
+                        disabled={throwingDice}
+                        onClick={() => throwDice('20')}
+                        >
+                        D20
+                    </button>
+
+
+
+                </div>
+            </div>
+
+            <div className='absolute bottom-0 left-0 p-4 w-full flex justify-between h-[70px] bg-gray-800'>
+                <button className='pointer-events-auto p-2 bg-red-700 rounded-lg text-white hover:bg-red-400' onClick={clearDice} disabled={dice.length === 0}>
+                    Clear
+                </button>
+
+
+                <div className="relative flex space-x-2">
+                    <button className="pointer-events-auto bg-gray-600 text-white p-2 rounded-lg hover:bg-purple-500" onClick={() => setShowTotals(!showTotals)}>
+                        { showTotals ? 'Hide' : 'Show'} History
+                    </button>
+
+                    <div className="text-gray-900 rounded-lg p-2 bg-white ">
+                        Total: {total}
+                    </div>
+
+                    {
+                        showTotals && (
+                            <div className="absolute bottom-[calc(100%+40px)] right-0 flex items-end flex-col space-y-2">
+                                {
+                                    totals.map((total, index) => {
+
+                                        const diceString = calculateDiceRolls(total.dice)
+
+                                        return (
+                                            <div key={index} className="w-max bg-gray-800 py-1 px-4 rounded-full text-white tracking-wider">
+                                                { diceString }- Total: {total.total}
+                                            </div>
+
+                                        )
+                                    })
+                                }
+
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -4,26 +4,32 @@ import { Environment, OrbitControls} from '@react-three/drei'
 import { useState } from 'react'
 
 import Experience from './Experience'
+import Interface from './Interface'
 
 export default function Scene() {
 
     const [ dice, setDice ] = useState([])
     const [ total, setTotal ] = useState(0)
     const [ throwingDice, setThrowingDice ] = useState(false)
+    const [ totals, setTotals ] = useState([])
 
     const throwDice = (sides) => {
         setDice([...dice, {
             id: Date.now(),
             sides: sides,
         }])
+        // Prevent the user from throwing multiple dice at once.
         setThrowingDice(true)
-        // set timeout to false after 1 second
         setTimeout(() => {
             setThrowingDice(false)
         }, 1000)
     }
 
     const clearDice = () => {
+        setTotals([...totals, {
+            total: total,
+            dice: dice,
+        }])
         setDice([])
         setTotal(0)
     }
@@ -31,88 +37,36 @@ export default function Scene() {
 
     return (
         <>
-            <Canvas
-                camera={{ position: [0, 10, 0], fov: 50 }}
-                shadows
-                >
-                <directionalLight 
-                    castShadow 
-                    position={ [ 0, 2, 1 ] } 
-                    intensity={ 1 } 
-                    shadow-normalBias={ 0.04 } // This setting fixes the shadow acne problem when multiple objects are casting shadows on each other.
-                    shadow-camera-top={ 10 }
-                    shadow-camera-right={ 10 }
-                    shadow-camera-bottom={ - 10 }
-                    shadow-camera-left={ - 10 }
-                    />
-                <Environment preset={'city'} />
-                {/* <OrbitControls /> */}
+            <div className="h-[calc(100%-70px)]">
+                <Canvas
+                    camera={{ position: [0, 10, 0], fov: 50 }}
+                    shadows
+                    >
+                    <directionalLight 
+                        castShadow 
+                        position={ [ 1, 2, 0.5 ] } 
+                        intensity={ 0.75 } 
+                        shadow-normalBias={ 0.04 }
+                        shadow-camera-top={ 10 }
+                        shadow-camera-right={ 10 }
+                        shadow-camera-bottom={ - 10 }
+                        shadow-camera-left={ - 10 }
+                        />
+                    <Environment preset={'apartment'} />
+                    {/* <OrbitControls /> */}
 
-                <Experience dice={dice} total={total} setTotal={setTotal} />
-            </Canvas>
-
-            <div className="absolute inset-0  pointer-events-none">
-                <div className="w-full p-4 flex items-center justify-between">
-                    <div className='flex space-x-4'>
-                        <button 
-                            className={`pointer-events-auto p-2 ${ throwingDice ? 'bg-gray-400 text-gray-500' : 'bg-gray-800 hover:bg-purple-500 text-white' } rounded-lg`}
-                            disabled={throwingDice}
-                            onClick={() => throwDice('4')}
-                            >
-                            D4
-                        </button>
-                        <button
-                            className={`pointer-events-auto p-2 ${ throwingDice ? 'bg-gray-400 text-gray-500' : 'bg-gray-800 hover:bg-purple-500 text-white' } rounded-lg`}
-                            disabled={throwingDice}
-                            onClick={() => throwDice('6')}
-                            >
-                            D6
-                        </button>
-                        <button
-                            className={`pointer-events-auto p-2 ${ throwingDice ? 'bg-gray-400 text-gray-500' : 'bg-gray-800 hover:bg-purple-500 text-white' } rounded-lg`}
-                            disabled={throwingDice}
-                            onClick={() => throwDice('8')}
-                            >
-                            D8
-                        </button>
-                        <button
-                            className={`pointer-events-auto p-2 ${ throwingDice ? 'bg-gray-400 text-gray-500' : 'bg-gray-800 hover:bg-purple-500 text-white' } rounded-lg`}
-                            disabled={throwingDice}
-                            onClick={() => throwDice('10')}
-                            >
-                            D10
-                        </button>
-                        <button
-                            className={`pointer-events-auto p-2 ${ throwingDice ? 'bg-gray-400 text-gray-500' : 'bg-gray-800 hover:bg-purple-500 text-white' } rounded-lg`}
-                            disabled={throwingDice}
-                            onClick={() => throwDice('12')}
-                            >
-                            D12
-                        </button>
-                        <button
-                            className={`pointer-events-auto p-2 ${ throwingDice ? 'bg-gray-400 text-gray-500' : 'bg-gray-800 hover:bg-purple-500 text-white' } rounded-lg`}
-                            disabled={throwingDice}
-                            onClick={() => throwDice('20')}
-                            >
-                            D20
-                        </button>
-
-
-
-                    </div>
-                </div>
-
-                <div className='absolute bottom-0 left-0 p-4 text-gray-900 w-full flex justify-between'>
-                    <button className='pointer-events-auto p-2 bg-red-400 rounded-lg text-white hover:bg-purple-500' onClick={clearDice}>
-                        Clear
-                    </button>
-
-
-                    <div className="text-gray-900 rounded-lg p-2 bg-white">
-                        Total: {total}
-                    </div>
-                </div>
+                    <Experience dice={dice} total={total} setTotal={setTotal} />
+                </Canvas>
             </div>
+
+            <Interface 
+                    throwingDice={throwingDice}
+                    throwDice={throwDice}
+                    clearDice={clearDice}
+                    dice={dice}
+                    total={total}
+                    totals={totals}
+                    />
         </>
     )
 }
