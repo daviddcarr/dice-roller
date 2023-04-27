f17146b786cdf672ae816a0fd46c3173c9c26a58
daviddcarr <daviddcarr13@gmail.com>
2023-04-27 14:34:43 -0500

    Add a loading screen.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Suspense } from 'react'
+
 import './App.css'
 
 import Scene from './components/Scene'
+import Loading from './components/Loading'
 
 function App() {
   return (
     <div className="fixed inset-0 bg-gray-900">
 
-      <Scene />
+      <Suspense fallback={<Loading />}>
+        <Scene />
+      </Suspense>
 
     </div>
   );
